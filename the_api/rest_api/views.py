1e34df6717a0c29179829c7a9f7c2ed236d69df9
serlus <serluscasas@gmail.com>
2020-09-11 08:24:32 -0300

    test for restapi

---
+++ the_api/rest_api/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+
+import json
+
+from rest_api.models import Car
+
+def index(request):
+    response = json.dumps([{}])
+    return HttpResponse(response, content_type="text/json")
+
+def get_car(request, car_name):
+    """[summary]
+
+    Args:
+        request ([type]): in 'car' is similar to SELECT * FROM Car WHERE name=car_name
+    """
+    if request.method == 'GET':
+        try:
+            car = Car.objects.get(name=car_name)
+            response = json.dumps([{ 'Car': car.name, 'Top Speed': car.top_speed}])
+        except:
+            response = json.dumps([{ 'Error': 'No car with that name'}])
+    return HttpResponse(response, content_type='text/json')
+
+@csrf_exempt
+def add_car(request):
+    if request.method == 'POST':
+        payload = json.loads(request.body)
+        car_name = payload['car_name']
+        top_speed = payload['top_speed']
+        car = Car(name=car_name, top_speed=top_speed)
+        try:
+            car.save()
+            response = json.dumps([{ 'Success': 'Car added successfully!'}])
+        except:
+            response = json.dumps([{ 'Error': 'Car could not be added!'}])
+    return HttpResponse(response, content_type='text/json')
